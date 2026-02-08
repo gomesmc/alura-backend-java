bf52de0a316a6c930ec5b4371be1c9ac3cd2075c
Maria Clara <gclara_@hotmail.com>
2026-02-08 09:58:53 -0300

    feature: Continue exercise 6

--- src/br/com/cursoalura/exercicios/nivel2/orientacaoaobjetos/pagina08/Servico.java
+++ src/br/com/cursoalura/exercicios/nivel2/orientacaoaobjetos/pagina08/Servico.java
@@ -1,16 +1,29 @@
 package br.com.cursoalura.exercicios.nivel2.orientacaoaobjetos.pagina08;
 
-public class Servico implements Vendavel{
+public class Servico implements Vendavel {
 
     @Override
     public void precoTotalDoProduto(int quantidadeComprada) {
-        ~~~~
+        System.out.println("Use precoTotalHoraTrabalhada(double)");
     }
 
     @Override
     public double aplicaDesconto() {
         return 0;
     }
+
+    public void precoTotalDoProduto(double precoHoraTrabalhada){
+        int totalHoraTrabalhada =8;
+        double precoTotal = precoHoraTrabalhada * totalHoraTrabalhada;
+        System.out.println("Total: " + precoTotal);
+    }
+
+    public static void main(String[] args) {
+        Servico servico = new Servico();
+        servico.precoTotalDoProduto(10);
+        servico.precoTotalDoProduto(8.0);
+    }
+
 }
 
 
